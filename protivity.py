3fe8fdbbd642651725df55b0b573102c04001200
BeatriceBa <beabaldassarre@outlook.com>
2025-03-27 12:19:59 +0100

    Aggiunta funzione anche a protivity.py

--- protivity.py
+++ protivity.py
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 def FedericoFunction():
     print("Modifica di Federico")
-=======
 
 def gabriele_function(n):
     if n %2 == 0:
         print('è divisibile per 2')
 
->>>>>>> ba073059c7331fb54dda345b51f349fa9e135c14
+def BeatriceFunction():
+    print("Prova dopo pull")
