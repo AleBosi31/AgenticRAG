03fc1bd672385da715cec2ca8d96d82d98ca8111
Abdelali Hassouna <109026943+abdelalihassouna@users.noreply.github.com>
2025-03-27 12:40:46 +0100

    Update protivity.py by Ali

--- protivity.py
+++ protivity.py
@@ -3,7 +3,6 @@
 def silviaFunction():
     print("Silvia's function")
 
-
 def FedericoFunction():
     print("Modifica di Federico")
 
@@ -11,11 +10,8 @@ def gabriele_function(n):
     if n %2 == 0:
         print('è divisibile per 2')
 
- HEAD
 def aleb_function():
     print("function")
 
 def BeatriceFunction():
     print("Prova dopo pull")
-
-49ad53f805852e49fb17a81c6acc61d4983104c9
